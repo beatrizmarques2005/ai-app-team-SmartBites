a1568f7a89b0d0845f9769e21910e4d88ee2504e
Constança Pereira da Silva <20231720@novaims.unl.pt>
2025-11-18 23:17:21 +0000

    tweaks in login page

--- streamlit/login.py
+++ streamlit/login.py
@@ -1,23 +1,33 @@
 import streamlit as st
+from pages import profile
+
+
 
 
 USER_CREDENTIALS = {
     'bitamima': '2005#!'
 }
 
+st.set_page_config(page_title='Login', initial_sidebar_state = 'collapsed', layout = 'centered')
 
-def login_page():
-    st.title('Login to your Smart Bites Page!')
+# def login_page():
+st.title('Login to your Smart Bites Page!')
 
-    username = st.text_input('Username')
-    password = st.text_input('Password', type = 'password')
+username = st.text_input('Username')
+password = st.text_input('Password', type = 'password')
 
-    if st.button('Login'):
-        if username in USER_CREDENTIALS and USER_CREDENTIALS[username] == password:
-            st.session_state['logged_in'] = True
-            st.session_state['username'] = username
-            st.success('Logged in successfully')
-            st.rerun()
+if st.button('Login'):
+    if username in USER_CREDENTIALS and USER_CREDENTIALS[username] == password:
+        st.session_state['logged_in'] = True
+        st.session_state['username'] = username
+        st.success('Logged in successfully')
+        # st.rerun()~
+        profile.profile_page()
         
-        else:
-            st.error('Invalid credentials')
+    
+    else:
+        st.error('Invalid credentials')
+
+st.markdown('Don\'t have an account?')
+if st.button('Sign Up', type = 'tertiary'):
+    st.markdown('Sign Up functionality is not implemented yet.')
---
+++ streamlit/pages/profile.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+
+def profile_page():
+    st.set_page_config(page_title='User Profile', page_icon='👤')
+
+    st.title('User Profile')
+    st.subheader('Hello, Francisca!')
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.image('https://static.vecteezy.com/system/resources/previews/019/879/186/original/user-icon-on-transparent-background-free-png.png')
+
+
+    with col2:
+        st.markdown(f'**Name**: {st.session_state.username}')
+        st.markdown('**Age**: 20')
+        st.markdown('**Household Size**: 2')
+        st.markdown('**Alergens/Intolerances**: Lactose')
