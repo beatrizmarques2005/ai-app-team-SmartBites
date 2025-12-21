6817f12bc5d89cbc58aa67ea0498858a1a2b5353
Mariana Calais-Pedro <mcalaispedro@gmail.com>
2025-12-21 02:04:59 +0000

    plannner streamlit running? review please!!

--- streamlit/pages_/planner2.py
+++ streamlit/pages_/planner2.py
@@ -1,4 +1,4 @@
-import streamlit as st
+'''import streamlit as st
 from datetime import datetime, timedelta
 from streamlit_calendar import calendar
 #pip install streamlit-calendar
@@ -50,4 +50,4 @@ def weekly_planner_page():
         st.markdown("---")
             # st.button("Close", key=f"close_{event_data['id']}", on_click=lambda: st.session_state.update({'clicked_event_details': None}))
     else:
-        st.info('Click on an event to see the details here.')
+        st.info('Click on an event to see the details here.')'''
